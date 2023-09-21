7c7f69845ea8af2e7983190986ca37b14884e22c
Minh Nguyen <nguyenngocminh190901@gmail.com>
2023-09-21 14:04:24 +0700

    update image uploader

--- src/app/shared/components/image-uploader/image-uploader.component.ts
+++ src/app/shared/components/image-uploader/image-uploader.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DialogModule } from 'primeng/dialog';
 import { FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
 import { FileService } from '@/services/file.service';
+import { MessageService } from 'primeng/api';
+import { NgIconsModule } from '@ng-icons/core';
 
 @Component({
   selector: 'app-image-uploader',
   standalone: true,
-  imports: [CommonModule, DialogModule, FileUploadModule],
+  imports: [CommonModule, DialogModule, FileUploadModule, NgIconsModule],
   templateUrl: './image-uploader.component.html',
   styleUrls: ['./image-uploader.component.scss'],
 })
@@ -15,18 +17,22 @@ export class ImageUploaderComponent {
   @Output() onUploadSuccess = new EventEmitter();
   visible: boolean = false;
   uploadedFiles: any[] = [];
+  failedFiles: any[] = [];
   @Input() multiple: boolean = true;
   @Input() showLoadingProgress: boolean = true;
   @Input() showLoadingProgressInModal: boolean = false;
   @Input() previewWidth: number = 50;
 
-  constructor(private fileService: FileService) {}
+  constructor(
+    private fileService: FileService,
+    private messageService: MessageService
+  ) {}
 
   showDialog() {
     this.visible = true;
   }
 
-  onUpload(event: FileUploadHandlerEvent) {
+  onUpload(event: FileUploadHandlerEvent, uploadRef: any) {
     if (!this.showLoadingProgressInModal) {
       this.visible = false;
     }
@@ -36,14 +42,35 @@ export class ImageUploaderComponent {
         name: file.name,
         id: index,
       });
-      this.fileService.uploadFile(file).subscribe((res) => {
-        if (res.success) {
+      this.fileService.uploadFile(file).subscribe({
+        next: (res) => {
+          if (res.success) {
+            this.uploadedFiles = this.uploadedFiles.filter(
+              (item) => item.id !== index
+            );
+            this.onUploadSuccess.emit(res.data.media);
+          }
+        },
+        error: (error) => {
+          this.failedFiles.push({ name: file.name, id: index });
           this.uploadedFiles = this.uploadedFiles.filter(
             (item) => item.id !== index
           );
-          this.onUploadSuccess.emit(res.data.media);
-        }
+          if (this.showLoadingProgressInModal) {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Upload Failed',
+              detail: 'The image is not uploaded',
+            });
+            this.visible = false;
+            uploadRef.clear();
+          }
+        },
       });
     });
+  }
+
+  removeFile(index: any) {
+    this.failedFiles = this.failedFiles.filter((item) => item.id !== index);
   }
 }
