6334e1ff58926078ac53edcf12dcf5f7aebfa049
Minh <nguyenngocminh190901@gmail.com>
2023-10-01 13:27:23 +0700

    add friend list

---
+++ src/app/modules/socials/components/friends/friend-list/friend-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FriendCardComponent,
+  FriendCardTypeEnum,
+} from '@/modules/socials/components/friend-card/friend-card.component';
+import { Store } from '@ngrx/store';
+import { selectMyFriends } from '@/core/store/friend/friend.selectors';
+import { FriendService } from '@/services/friend.service';
+
+@Component({
+  selector: 'app-friend-list',
+  standalone: true,
+  imports: [CommonModule, FriendCardComponent],
+  templateUrl: './friend-list.component.html',
+  styleUrls: ['./friend-list.component.scss'],
+})
+export class FriendListComponent implements OnInit {
+  constructor(private store: Store, private friendService: FriendService) {}
+  FriendCardTypeEnum = FriendCardTypeEnum;
+  friends$ = this.store.select(selectMyFriends);
+
+  ngOnInit(): void {
+    this.friendService.getMyFriends().subscribe();
+  }
+}
